5003b17191bfebc99926435e3101897bc8a88c81
kieran gillen <kgillen@gmail.com>
2016-12-09 10:37:15 -0500

    server running

---
+++ server.js
@@ -0,0 +1,32 @@
+'use strict'
+require('dotenv').config({ silent: true });
+const express = require('express');
+const logger  = require('morgan');
+const path    = require('path');
+const cookieParser  = require('cookie-parser');
+const bodyParser = require('body-parser');
+const app     = express();
+const PORT    = process.argv[2] || process.env.PORT || 3000;
+// const roverRouter = require('./routes/rover');
+// const visionRouter = require('./routes/vision');
+// const bingRouter = require('./routes/bing');
+// const usersRouter   = require('./routes/api/users');
+// const authRouter    = require('./routes/api/auth');
+// const imagesRouter        = require('./routes/images');
+
+app.use(logger('dev'));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(cookieParser());
+
+app.use(bodyParser.json());
+
+// app.use('/rover', roverRouter);
+// app.use('/vision', visionRouter);
+// app.use('/bing', bingRouter);
+// app.use('/api/users', usersRouter);
+// app.use('/api/auth', authRouter);
+// app.use('/images', imagesRouter)
+
+app.listen(PORT, () => console.log('server here! listening on', PORT));
---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render(){
+    return (
+      <div>
+        <p>test working</p>
+      </div>
+   );
+ }
+}
+
+export default App;
